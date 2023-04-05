52cb94547cba4d2087828afa6221f33826120807
hisham <h.wattar@netro.xyz>
2023-04-06 00:36:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// Returns the matching list of drivers
+function findMatching(array, string) {
+  return array.filter(function (name) {
+    return name.toUpperCase() === string.toUpperCase()
+  })
+}
+
+// Returns all drivers whose names begin with the provided letters
+function fuzzyMatch(array, string) {
+  return array.filter(function (name) {
+    return name.startsWith(string)
+  })
+}
+
+// Return each element whose name property matches the provided string argument
+function matchName(array, string) {
+  return array.filter(function (item) {
+    return item.name === string
+  })
+}
